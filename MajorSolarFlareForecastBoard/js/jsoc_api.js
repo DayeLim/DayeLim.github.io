edb1588eb9028fa81e3a7c08d1f19fe3a67c2b1e
DayeLim <DayeLim@github.io>
2021-05-12 23:29:02 +0900

    possible?

--- MajorSolarFlareForecastBoard/js/jsoc_api.js
+++ MajorSolarFlareForecastBoard/js/jsoc_api.js
@@ -1,6 +1,5 @@
-const request = new XMLHttpRequest();
 const title = 'Major Solar Flare Forecast Board';
-const resourUrl = 'https://Dayelim.github.io/MajorSolarFlareForecastBoard/resource/';
+const resourUrl = '../MajorSolarFlareForecastBoard/resource/';
 const mharp = resourUrl + 'hmi_mharp_720s_nrt.html';
 var harp_addr_head = resourUrl + 'hmi_sharp_720s_nrt[';
 var harp_addr_foot = '].html';
@@ -12,52 +11,81 @@ addLoadEvent(function() {
     name.innerHTML = title;
 });
 
-request.open('POST', mharp, true);
-addLoadEvent_http(function () {
-    addLoadEvent(function () {
-        var jsonMharp = JSON.parse(request.responseText);
-        // console.log(jsonMharp);
-        for(var j = 0; j < jsonMharp.count; j++) {
-            var harp_addr = harp_addr_head+jsonMharp.keywords[1].values[j]+harp_addr_foot;
-            // console.log(harp_addr);
-            // console.log(Object.keys(jsonMharp.keywords).length);
-                request.open('POST', harp_addr, false);
-                addLoadEvent_http(function () {
-                    console.log(j);
-                    var jsonHarp = JSON.parse(request.responseText);
+makeRequest();
 
-                    var tbody = document.getElementsByClassName('tbody')[0];
+function makeRequest() {
+    httpRequest = new XMLHttpRequest();
 
-                    var tr_value = document.createElement('tr');
-                    tr_value.className = 'tr';
+    if(!httpRequest) {
+        return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', mharp, true);
+    httpRequest.send();
+}
 
-                    tbody.appendChild(tr_value);
-
-                    for(var i = 0; i < Object.keys(jsonMharp.keywords).length; i++) {
-                        var td = document.createElement('td');
-                        td.innerHTML = jsonMharp.keywords[i].values[j];
-                        tr_value.appendChild(td);
-                    }
-
-                    for(var i = 0; i < Object.keys(jsonHarp.keywords).length - 2; i++) {
-                        var td = document.createElement('td');
-                        td.innerHTML = jsonHarp.keywords[i+2].values[jsonHarp.keywords[i+2].values.length-1];
-                        tr_value.appendChild(td);
-
-                        var td_p = document.createElement('td');
-                        td_p.innerHTML = jsonHarp.keywords[i+2].values[jsonHarp.keywords[i+2].values.length-1];
-                        tr_value.appendChild(td_p);
-                    }
-
-                    // console.log(j, jsonHarp);
-                });
-                request.send();
-
-            // console.log(harp_addr);
+function alertContents() {
+    try {
+        if(httpRequest.readyState === XMLHttpRequest.DONE) {
+            if(httpRequest.status === 200) {
+                var jsonMharp = JSON.parse(httpRequest.responseText);
+                var j = 0;
+                var harp_addr = harp_addr_head+jsonMharp.keywords[1].values[j]+harp_addr_foot;
+                console.log(harp_addr);
+            } else {
+                alert("problem");
+            }
         }
-    });
-});
-request.send();
+    } catch(e) {
+        console.log('Caught Exception: ' + e.description);
+    }    
+}
+// request.open('POST', mharp, true);
+// addLoadEvent_http(function () {
+//     addLoadEvent(function () {
+//         var jsonMharp = JSON.parse(request.responseText);
+//         // console.log(jsonMharp);
+//         for(var j = 0; j < jsonMharp.count; j++) {
+//             var harp_addr = harp_addr_head+jsonMharp.keywords[1].values[j]+harp_addr_foot;
+//             // console.log(harp_addr);
+//             // console.log(Object.keys(jsonMharp.keywords).length);
+//                 request.open('POST', harp_addr, false);
+//                 addLoadEvent_http(function () {
+//                     console.log(j);
+//                     var jsonHarp = JSON.parse(request.responseText);
+
+//                     var tbody = document.getElementsByClassName('tbody')[0];
+
+//                     var tr_value = document.createElement('tr');
+//                     tr_value.className = 'tr';
+
+//                     tbody.appendChild(tr_value);
+
+//                     for(var i = 0; i < Object.keys(jsonMharp.keywords).length; i++) {
+//                         var td = document.createElement('td');
+//                         td.innerHTML = jsonMharp.keywords[i].values[j];
+//                         tr_value.appendChild(td);
+//                     }
+
+//                     for(var i = 0; i < Object.keys(jsonHarp.keywords).length - 2; i++) {
+//                         var td = document.createElement('td');
+//                         td.innerHTML = jsonHarp.keywords[i+2].values[jsonHarp.keywords[i+2].values.length-1];
+//                         tr_value.appendChild(td);
+
+//                         var td_p = document.createElement('td');
+//                         td_p.innerHTML = jsonHarp.keywords[i+2].values[jsonHarp.keywords[i+2].values.length-1];
+//                         tr_value.appendChild(td_p);
+//                     }
+
+//                     // console.log(j, jsonHarp);
+//                 });
+//                 request.send();
+
+//             // console.log(harp_addr);
+//         }
+//     });
+// });
+// request.send();
 
 function addLoadEvent(func) {
     var oldonload = window.onload;
@@ -71,17 +99,17 @@ function addLoadEvent(func) {
     }
 }
 
-function addLoadEvent_http(func) {
-    var oldonload = request.onload;
-    if(typeof request.onload != 'function') {
-        request.onload = func;
-    } else {
-        request.onload = function() {
-            oldonload();
-            func();
-        }
-    }
-}
+// function addLoadEvent_http(func) {
+//     var oldonload = request.onload;
+//     if(typeof request.onload != 'function') {
+//         request.onload = func;
+//     } else {
+//         request.onload = function() {
+//             oldonload();
+//             func();
+//         }
+//     }
+// }
 
 
 // fetch('http://jsoc.stanford.edu/cgi-bin/ajax/jsoc_info?ds=hmi.mharp_720s_nrt[][$]&op=rs_list&key=HARPNUM,NOAA_ARS,T_REC')
